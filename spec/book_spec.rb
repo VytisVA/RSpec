a5d3726e9fa60c3ea81b3d6611f6d14a7f5bba35
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-18 17:08:56 +0000

    Final push since tutorial is out-dated

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,15 @@
-require './spec_helper'
+require 'spec_helper'
 
 describe Book do
 
 	before :each do
-		@book = Book.new "Title", "Author", :category	
+		@book = Book.new	
 	end	
 
 	describe "#new" do
 		it "retuns a new book object" do
 			@book.should be_an_instance_of Book 
-	end	
-
-end
+		end	
+	end
+	
+end	
